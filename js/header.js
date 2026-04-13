afac37811af3173096b18dbaad8981df6ee9e15a
caioxdev <caioseabradequeiroz2006@gmail.com>
2026-04-13 15:58:13 -0300

    ajustando header e fazendo parte do main

---
+++ js/header.js
@@ -0,0 +1,7 @@
+export const initHeader = () => {
+  const header = document.querySelector('header');
+
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 0);
+  })
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { iniciarSwup } from './swup.js';
 import { initThemeSwitcher } from './modo-claro-escuro.js';
+import { initHeader } from './header.js';
 
 iniciarSwup();
 
 initThemeSwitcher();
+
+initHeader();
 
 document.addEventListener('swup:contentReplaced', () => {
   initThemeSwitcher();
