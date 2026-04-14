7cf0b90f7d71e921d71fbdaa8db8b22129f5ab31
caioxdev <caioseabradequeiroz2006@gmail.com>
2026-04-13 22:46:30 -0300

    swup funcionando

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
-import { iniciarSwup } from './swup.js';
-import { initThemeSwitcher } from './modo-claro-escuro.js';
-import { initHeader, restaurarActive, activeNav } from './header.js';
+import { iniciarSwup } from "./swup.js";
+import { initThemeSwitcher } from "./modo-claro-escuro.js";
+import { initHeader, restaurarActive } from "./header.js";
 
 const swup = iniciarSwup();
 
-initThemeSwitcher();
-initHeader();
-
-document.addEventListener('DOMContentLoaded', () => {
-  restaurarActive();
-});
-
-const botao = document.querySelector('.btn-iniciar');
-
-if (botao) {
-  botao.addEventListener('click', (event) => {
-    event.preventDefault();
-    activeNav();
-    window.location.href = 'pages/quiz.html';
-  });
-}
-
-
-swup.hooks.on('page:view', () => {
+function initAll() {
   initThemeSwitcher();
   initHeader();
   restaurarActive();
-})
+}
+
+initAll();
+
+swup.hooks.on('page:view', () => {
+  initAll();
+});
--- js/swup.js
+++ js/swup.js
@@ -3,7 +3,10 @@ import Swup from 'https://unpkg.com/swup@4?module';
 export const iniciarSwup = () => {
   const swup = new Swup({
     containers: ['#swup'],
-    linkSelector: 'a[href]'
+    animateHistoryBrowsing: true,
+    cache: false,
+    linkSelector: 'a[href]:not([target="_blank"]):not([href^="#"])',
   });
+
   return swup;
-}
+};
